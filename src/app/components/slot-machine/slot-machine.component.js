32b8cefd9bd57f2c4bff90c732be4b36f0b4fc05
tanmayjuneja8 <tanmayjuneja8@gmail.com>
2022-10-30 23:25:22 +0530

    43

--- src/app/components/slot-machine/slot-machine.component.js
+++ src/app/components/slot-machine/slot-machine.component.js
@@ -174,8 +174,8 @@ export class SlotMachine {
         const deltaTime = now - lastUpdate;
         const deltaAlpha = deltaTime * speed;
         console.log('2');
-        if (this.isPaused) {
-            console.log(this.isPaused);
+        if (currentReel === null || this.isPaused) {
+            this.isPaused = false;
             console.log(currentReel);
             console.log('3');
             return;
