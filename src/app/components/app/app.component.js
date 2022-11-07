89345be2d7549e49d02ae3b75ba295fc4aea09b7
tanmayjuneja8 <tanmayjuneja8@gmail.com>
2022-11-08 00:34:56 +0530

    Blozum

--- src/app/components/app/app.component.js
+++ src/app/components/app/app.component.js
@@ -224,7 +224,7 @@ export class App {
         const linkedinBtn = document.getElementById('linkedin');
         const whatsappBtn = document.getElementById('wa');
         const postUrl = 'https://blozum.com/';
-        const postTitle = encodeURI('Play this game and use my Blozum referral code to win cash, vouchers, and exciting prizes like I did on the Blozum website. My Referral code : ' + uniqueCode + ' Blozom Website:');
+        const postTitle = encodeURI('Play this game and use my Blozum referral code to win cash, vouchers, and exciting prizes like I did on the Blozum website. My Referral code : ' + uniqueCode + ' Blozum Website:');
         document.getElementById('ref_copy').addEventListener('copy_ref', navigator.clipboard.writeText('Play this game and use my Blozum referral code to win cash, vouchers, and exciting prizes like I did on the Blozum website. My Referral code : ' + uniqueCode + ' Blozom Website: ' + postUrl));
 
         facebookBtn.setAttribute(
